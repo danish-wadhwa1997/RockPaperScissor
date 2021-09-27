2591f5e712a30fba4c501fd126bfc2a6269534da
DANISH WADHWA <danish.wadhwa1997@gmail.com>
2021-09-27 18:32:08 +0530

    Add Navigation button to go to rules setup

--- src/components/organisms/AddChoices.tsx
+++ src/components/organisms/AddChoices.tsx
@@ -3,17 +3,25 @@ import MainHeadings from "../atoms/MainHeadings";
 import { Choice } from "../../configuration/Interfaces";
 import Choices from "../molecules/Choices";
 import ChoiceView from "../atoms/ChoiceView";
+import PrimaryButton from "../atoms/PrimaryButton";
+import { useHistory } from "react-router-dom";
 const AddChoices = () => {
   const [choices, setChoices] = useState<Choice[]>([]);
-
+  const history = useHistory();
   const onAdd = (obj: Choice) => {
     let newOptions = [...choices];
     newOptions.push(obj);
     setChoices(newOptions);
   };
+
   const handleChoiceDelete = (label: string): any => {
     let newOptions = choices.filter((item) => item.label !== label);
     setChoices(newOptions);
+  };
+
+  const handleNext = () => {
+    console.log(choices);
+    history.replace("/game-setup/setup-rules");
   };
   return (
     <div>
@@ -27,6 +35,8 @@ const AddChoices = () => {
           />
         ))}
       <Choices onAdd={onAdd} />
+
+      <PrimaryButton onClick={handleNext}>Next</PrimaryButton>
     </div>
   );
 };
