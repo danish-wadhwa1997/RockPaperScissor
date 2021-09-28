3f79d0aaf04ea01f35ac4b10081408ea78aef9aa
DANISH WADHWA <danish.wadhwa1997@gmail.com>
2021-09-28 23:39:33 +0530

    router shift

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,10 @@
 import React from "react";
-import { Switch, Route, Redirect } from "react-router-dom";
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Redirect,
+} from "react-router-dom";
 import GamePlay from "./components/pages/GamePlay";
 import GameSetup from "./components/pages/GameSetup";
 import GameTypeProvider from "./components/context/GameTypeContext";
@@ -7,24 +12,26 @@ import ChoiceProvider from "./components/context/ChoiceContext";
 import RulesProvider from "./components/context/RulesContext";
 function App() {
   return (
-    <GameTypeProvider>
-      <ChoiceProvider>
-        <RulesProvider>
-          <div className="container-fluid py-5">
-            <h1 className="display-1 mb-5 text-center">
-              Rock Paper Scissor &amp; more..
-            </h1>
-            <Switch>
-              <Route path="/game-setup" component={GameSetup} />
-              <Route exact path="/game-play" component={GamePlay} />
-              <Route path="/**">
-                <Redirect to="/game-setup" />
-              </Route>
-            </Switch>
-          </div>
-        </RulesProvider>
-      </ChoiceProvider>
-    </GameTypeProvider>
+    <Router>
+      <GameTypeProvider>
+        <ChoiceProvider>
+          <RulesProvider>
+            <div className="container-fluid py-5">
+              <h1 className="display-1 mb-5 text-center">
+                Rock Paper Scissor &amp; more..
+              </h1>
+              <Switch>
+                <Route path="/game-setup" component={GameSetup} />
+                <Route exact path="/game-play" component={GamePlay} />
+                <Route path="/**">
+                  <Redirect to="/game-setup" />
+                </Route>
+              </Switch>
+            </div>
+          </RulesProvider>
+        </ChoiceProvider>
+      </GameTypeProvider>
+    </Router>
   );
 }
 
